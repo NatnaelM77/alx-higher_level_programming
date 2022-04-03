06e398fa73d37598d4dbe1f8a2a5bf54c42a30b9
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-03 20:19:31 +0300

    Update via upload

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
 takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response.
+email as a parameter, and displays the body of the response
 """
 if __name__ == "__main__":
+    import urllib.parse as parse
+    import urllib.request as request
     from sys import argv
-    import urllib.parse
-    import urllib.request
+
     values = {'email': argv[2]}
-    data = urllib.parse.urlencode(values).encode('utf-8')
-    req = urllib.request.Request(argv[1], data)
-    with urllib.request.urlopen(req) as response:
+    data = parse.urlencode(values).encode('utf-8')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
         print(f'{response.read().decode("utf-8")}')
