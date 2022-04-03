41d14e9f4be24c975f2935046f86314904f4e471
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-03 20:16:15 +0300

    Update via upload

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,7 @@
 takes in a URL and an email, sends a POST request to the passed URL with the
 email as a parameter, and displays the body of the response.
 """
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
     import urllib.parse
     import urllib.request
