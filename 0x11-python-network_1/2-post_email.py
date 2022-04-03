e34eedb164c5f10d0394de10a4ad5d5418d2ffe8
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-03 19:36:15 +0300

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 '''
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
+takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter
 '''
 
 from sys import argv
