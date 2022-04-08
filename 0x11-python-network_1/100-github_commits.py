18f839ef134ca86aa6826e5b1e9a8f47123323f5
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-08 11:06:57 -0700

    update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,12 +4,12 @@
  takes 2 arguments in order to solve this challenge.
 '''
 
-import requests
-from sys import argv
-
 if __name__ == '__main__':
-    req = requests.get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}'
-                       f'/commits')
-    commits = req.json()
-    for i in commits[:10]:
-        print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
+    import requests
+    from sys import argv
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
