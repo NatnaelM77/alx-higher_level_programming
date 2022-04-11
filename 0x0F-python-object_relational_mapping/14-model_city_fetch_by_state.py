3e154af194369ef5893c80135d469845a2824be8
NatnaelM77 <natnaelmehari77@gmail.com>
2022-04-11 12:15:38 +0300

    Update 14-model_city_fetch_by_state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Print all City objs from db 'hbtn_0e_14_usa'
-Sort in ascending order by cities.id
-Display results as "<state name>: (<city id>) <city name>"
-Script should take 3 args: username, pw, and db name
-Must use SQLAlchemy
-"""
+""" Print all City objs from db 'hbtn_0e_14_usa' """
 
 from sys import argv
 from model_city import Base, City
