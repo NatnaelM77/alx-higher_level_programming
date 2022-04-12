f42d573aee31b5f2be1441faef3993424a412472
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-12 04:19:35 -0700

    Fix Linter error

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,13 +3,11 @@
 const request = require('request');
 request.get(process.argv[2], function (err, res, body) {
   if (err) throw err;
-  let obj = {}
-  let data = JSON.parse(body)
-  for (let i = 0; i < data.length; i++)
-  {
-    if (data[i].completed == true)
-    {
-       obj[data[i].userId] = data[i].id;
+  const obj = {};
+  const data = JSON.parse(body);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].completed === true) {
+      obj[data[i].userId] = data[i].id;
     }
   }
   console.log(obj);
