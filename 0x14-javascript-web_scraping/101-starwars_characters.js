1ecd9b499a30f2ffad3cf565200bd18daefd1432
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-12 08:32:13 -0700

    fix linter error

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,10 +4,9 @@ const request = require('request');
 request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (err, res, body) {
   if (err) throw err;
 
-  let data = JSON.parse(body);
+  const data = JSON.parse(body);
 
-  for (let url of data.characters)
-  {
+  for (const url of data.characters) {
     request.get(url, function (err, res, body) {
       if (err) throw err;
       console.log(JSON.parse(body).name);
