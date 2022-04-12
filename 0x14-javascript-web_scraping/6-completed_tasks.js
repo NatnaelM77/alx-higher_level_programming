3bc978de5d5dcf6e22a8cae6c4bd5b3806d84360
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-12 10:13:07 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,15 @@
 
 const request = require('request');
 request.get(process.argv[2], function (err, res, body) {
-  let count = 0; let end = 20;
+  let tmp = '';
   const obj = {};
   if (err) throw err;
 
   const data = JSON.parse(body);
   for (let i = 0; i < data.length; i++) {
-    if (data[i].completed === true) {
-      count += 1;
-      obj[data[i].userId] = count;
-    }
-    if (data[i].id === end) {
-      count = 0;
-      end += 20;
+    tmp = data[i].userId;
+    if (data[i].completed) {
+      obj[data[i].userId] = (obj[tmp] || 0) + 1;
     }
   }
   console.log(obj);
