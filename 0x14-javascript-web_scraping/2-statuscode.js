55b9984a71e32d78fb203f8954941f496519f1e1
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-12 03:13:24 -0700

    Fix linter error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,6 @@
 
 const request = require('request');
 request.get(process.argv[2], function (err, res) {
+  if (err) throw err;
   console.log(`code: ${res && res.statusCode}`);
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,6 @@
 
 const request = require('request');
 request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (err, res, body) {
+  if (err) throw err;
   console.log(JSON.parse(body).title);
 });
