48d551b265cc37f27f02c6334e02e733a6764003
NantaelM77 <natnaelmehari77@gmail.com>
2022-04-12 08:18:46 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,20 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get('https://swapi-api.hbtn.io/api/people/18', function (err, res, body) {
+request.get(process.argv[2], function (err, res, body) {
+  let count = 0;
+  const data = JSON.parse(body);
+  const reg = /18/g;
+
   if (err) throw err;
-  console.log(JSON.parse(body).films.length);
+
+  for (let i = 0; i < data.results.length; i++) {
+    for (let j = 0; j < data.results[i].characters.length; j++) {
+      if (reg.test(data.results[i].characters[j])) {
+        count += 1;
+      }
+    }
+  }
+
+  console.log(count);
 });
