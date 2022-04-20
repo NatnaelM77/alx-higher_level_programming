d78aa7c36c6f35f8977374e5791375f6620f9618
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-20 15:31:20 +0300

    Say hello to everybody!

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
     $('INPUT#btn_translate').click(function () {
             let lang = $('INPUT#language_code').val();
             let request = $.get(`https://fourtonfish.com/hellosalut/?lang=${lang}`,
