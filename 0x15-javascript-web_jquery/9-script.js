17781df3b0330d605dc402f19aa60664c0d9922f
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-20 13:30:46 -0700

    Say Hello!

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-    let request = $.get('https://fourtonfish.com/hellosalut/?lang=fr',
-        function (response) {
-            $('DIV#hello').text(response.hello)
-        }
-    );
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr',
+    function (response) {
+      $('DIV#hello').text(response.hello);
+    }
+  );
 });
