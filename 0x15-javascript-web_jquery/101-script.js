d7633394650bb48cd029a2a38859c08d50b06a35
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-20 14:34:18 +0300

    List, add, remove

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,13 +1,15 @@
-$('DIV#add_item').click(function() {
-    let list_item = $('<li>', {text: 'Item'});
-    $('UL.my_list').append(list_item);
-});
+document.addEventListener('DOMContentLoaded', function () {
+    $('DIV#add_item').click(function () {
+        let list_item = $('<li>', {text: 'Item'});
+        $('UL.my_list').append(list_item);
+    });
 
-$('DIV#remove_item').click(function() {
-    let list_item = $('UL.my_list li:last-child');
-    $('UL.my_list').remove(list_item);
-});
+    $('DIV#remove_item').click(function () {
+        let list_item = $('UL.my_list li:last-child');
+        $('UL.my_list').remove(list_item);
+    });
 
-$('DIV#clear_list').click(function() {
-    $('UL.my_list').html('');
+    $('DIV#clear_list').click(function () {
+        $('UL.my_list').html('');
+    });
 });
