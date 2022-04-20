6627cca6d79f551b43aa39a883de113c93cdd70e
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-20 15:28:43 +0300

    And press ENTER

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,10 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-    $('INPUT#btn_translate').click(function () {
-        let lang = $('INPUT#language_code').val();
-        let request = $.get(`https://fourtonfish.com/hellosalut/?lang=${lang}`,
-            function (response) {
-                $('DIV#hello').text(response.hello)
+$(document).ready(function () {
+    $('INPUT#btn_translate').on('click', function () {
+            let lang = $('INPUT#language_code').val();
+            let request = $.get(`https://fourtonfish.com/hellosalut/?lang=${lang}`,
+                function (response) {
+                    $('DIV#hello').text(response.hello)
+                }
+            );
+        },
+        'keypress', function (keyEvent) {
+            if (keyEvent.which == 13) {
+                let lang = $('INPUT#language_code').val();
+                let request = $.get(`https://fourtonfish.com/hellosalut/?lang=${lang}`,
+                    function (response) {
+                        $('DIV#hello').text(response.hello)
+                    }
+                );
             }
-        );
-    });
+        }
+    );
 });
