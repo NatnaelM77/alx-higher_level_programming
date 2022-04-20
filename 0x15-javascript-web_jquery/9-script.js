696335b030f825dc974d97fb920fa3003ee8e5c3
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-20 15:30:46 +0300

    Say Hello!

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
     let request = $.get('https://fourtonfish.com/hellosalut/?lang=fr',
         function (response) {
             $('DIV#hello').text(response.hello)
