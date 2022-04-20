e8300aacaf792cabfcaca71e627be9f1fa434ecf
natnaelm77 <natnaelmehari77@gmail.com>
2022-04-20 14:32:10 +0300

    List, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,13 @@
+$('DIV#add_item').click(function() {
+    let list_item = $('<li>', {text: 'Item'});
+    $('UL.my_list').append(list_item);
+});
+
+$('DIV#remove_item').click(function() {
+    let list_item = $('UL.my_list li:last-child');
+    $('UL.my_list').remove(list_item);
+});
+
+$('DIV#clear_item').click(function() {
+    $('UL.my_list').html('');
+});
