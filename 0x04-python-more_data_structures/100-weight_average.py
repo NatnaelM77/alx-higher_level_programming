402ff260e594ae3d2ddfbdc34f4ca530ff46bccc
NatnaelM77 <natnaelmehari77@gmail.com>
2022-06-15 14:19:11 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,10 +2,10 @@
 
 
 def weight_average(my_list=[]):
-	score = weight = 0
-	if not my_list:
-		return (0)
-	for t in my_list:
-		score += t[0] * t[1]
-		weight += t[1]
-	return (score / weight)
+    score = weight = 0
+    if not my_list:
+        return (0)
+    for t in my_list:
+        score += t[0] * t[1]
+        weight += t[1]
+    return (score / weight)
